46b551d39b935dccff940ac835012379b99ced45
toto <toto6321@qq.com>
2017-11-09 17:36:53 +0800

    #1 Preliminarily create the express project #2 organize the project #3 bring the bootstrap and jquery in

---
+++ app_backend/controllers/index.js
@@ -0,0 +1,9 @@
+/**
+ *   @author toto
+ *   @createdDate    2017-11-09 04:41 PM
+ *   @email  toto6321@qq.com
+ */
+
+module.exports.index = function (req, res, next) {
+    res.render('index', {title: 'Express'});
+};
