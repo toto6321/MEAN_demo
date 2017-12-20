7b2f1d5ebe0c3780546a465cbdbcf7ea51cb4dcc
toto <toto6321@qq.com>
2017-12-20 22:35:48 +0800

    DONE: #1 fix the problem by removing # from <a href='/#about'></a> because of the usage of $locationProvider.html5Mode(true)

--- app_client/views/about/about.controller.js
+++ app_client/views/about/about.controller.js
@@ -11,6 +11,10 @@ const aboutController = function () {
     content: 'Loc8r was created to help people find places to sit down and\n' +
     'get a bit of work done.'
   }
+  vm.pageHeader = {
+    title: 'Loc8r',
+    strapline: 'Find places to work with wifi near you!'
+  };
 }
 
 angular
